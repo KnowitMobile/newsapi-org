5b16f375fca6798c6363360d67d8b69bef84de8e
Magnus Wissler <magnus.wissler@knowit.se>
2019-09-29 15:26:06 +0200

    Generate absolute URL

--- Sources/NewsAPI.org/NewsAPI.swift
+++ Sources/NewsAPI.org/NewsAPI.swift
@@ -42,7 +42,7 @@ public final class NewsAPI {
         }
 
         func build() -> URLRequest {
-            var request = URLRequest(url: self.url.url!)
+            var request = URLRequest(url: self.url.url!.absoluteURL)
             if let apiKey = apiKey {
                 request.setValue(apiKey, forHTTPHeaderField: "Authorization")
             }
